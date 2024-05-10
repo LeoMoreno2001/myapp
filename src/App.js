5a70713bdb26ccd78f2d1d04eea19cc7797601c8
LeoMoreno2001 <leoladocta10@gmail.com>
2024-05-10 14:10:04 -0300

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <Button>This is a button from bootstrap</Button>
+        </p>  
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
